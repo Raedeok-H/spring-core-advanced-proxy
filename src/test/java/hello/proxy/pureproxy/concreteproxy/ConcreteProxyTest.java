bf7a44da8b36d139abef6708e8c1aed5c333ed09
Raedeok-H <raedeokhwang@gmail.com>
2024-03-29 08:49:55 +0900

    구체 클래스 기반 프록시 - 예제2

--- src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureproxy.concreteproxy;
 
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreteproxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -10,5 +11,17 @@ public class ConcreteProxyTest {
         ConcreteLogic concreteLogic = new ConcreteLogic();
         ConcreteClient client = new ConcreteClient(concreteLogic);
         client.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
+        client.execute();
+        //아래와 같은 개념으로 클래스 기반 프록시를 적용하는 것이다.
+        //ConcreteLogic에 할당할 수 있는 객체
+        //ConcreteLogic = concreteLogic (본인과 같은 타입을 할당)
+        //ConcreteLogic = timeProxy (자식 타입을 할당)
     }
 }
