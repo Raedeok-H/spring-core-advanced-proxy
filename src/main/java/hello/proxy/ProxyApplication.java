8621e9ae830863ec07ae781f244df5a807aaabf5
Raedeok-H <raedeokhwang@gmail.com>
2024-03-28 11:14:19 +0900

    예제 프로젝트 만들기 v2

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,13 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class) //해당 클래스를 스프링 빈으로 등록
+//@Import(AppV1Config.class) //해당 클래스를 스프링 빈으로 등록
+@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의 => app 패키지와 그 하위만 스캔 => 강의가 진행됨에 따라 import로 다른 것들을 설정하기 위해(전체를 스캔하는 것을 방지)
 public class ProxyApplication {
 
