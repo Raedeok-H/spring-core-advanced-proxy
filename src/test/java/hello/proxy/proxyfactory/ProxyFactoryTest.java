119c88ba70cb40db3f07d4390f05541ceee27522
Raedeok-H <raedeokhwang@gmail.com>
2024-04-02 15:08:18 +0900

    프록시 팩토리 - 예제 코드2

--- src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
+++ src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -1,6 +1,7 @@
 package hello.proxy.proxyfactory;
 
 import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ConcreteService;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +18,41 @@ public class ProxyFactoryTest {
     void interfaceProxy() {
         ServiceInterface target = new ServiceImpl();
         ProxyFactory proxyFactory = new ProxyFactory(target); // 적용할 클래스
+        proxyFactory.addAdvice(new TimeAdvice());   // 적용할 프록시 로직
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.save();
+
+        Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+
+    @Test
+    @DisplayName("구체 클래스만 있으면 CGLIB 사용")
+    void concreteProxy() {
+        ConcreteService target = new ConcreteService();
+        ProxyFactory proxyFactory = new ProxyFactory(target); // 적용할 클래스
+        proxyFactory.addAdvice(new TimeAdvice());   // 적용할 프록시 로직
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.call();
+
+        Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+
+    @Test
+    @DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB를 사용하고, 클래스 기반 프록시 사용")
+    void proxyTargetClass() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target); // 적용할 클래스
+        proxyFactory.setProxyTargetClass(true); // 인터페이스가 있어도 class기반으로 cglib를 사용하여 프록시를 만듦
         proxyFactory.addAdvice(new TimeAdvice());   // 적용할 프록시 로직
         ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
         log.info("targetClass={}", target.getClass());
